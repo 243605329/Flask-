3229be5bca7181f75fbb9a0ea929ca7d88ea79fe
243605329@qq.com <243605329@qq.com>
2025-02-21 18:12:19 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from flask import Flask,url_for
+from flask import Flask,url_for,render_template
 
 app = Flask(__name__)
 
+name	=	'Study Flask'
+movies	=	[
+ {'title':	'My	Neighbor	Totoro',	'year':	'1988'},
+ {'title':	'Dead	Poets	Society',	'year':	'1989'},
+ {'title':	'A	Perfect	World',	'year':	'1993'},
+ {'title':	'Leon',	'year':	'1994'},
+ {'title':	'Mahjong',	'year':	'1996'},
+ {'title':	'Swallowtail	Butterfly',	'year':	'1996'},
+ {'title':	'King	of	Comedy',	'year':	'1999'},
+ {'title':	'Devils	on	the	Doorstep',	'year':	'1999'},
+ {'title':	'WALL-E',	'year':	'2008'},
+ {'title':	'The	Pork	of	Music',	'year':	'2012'},
+ ]
+
+
 @app.route('/')
-def hello():
-    return	'<h1>Hello	Totoro!</h1><img	src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
-@app.route('/user/<name>')
-def user_page(name):
-    return	'<h1>Hello,	{name}!</h1>'
 
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello')) # 输出：/
-    print(url_for('user_page', name='greyli')) # 输出：/user/greyli
-    print(url_for('user_page', name='peter')) # 输出：/user/peter
-    print(url_for('test_url_for')) # 输出：/test
-    print(url_for('test_url_for',num=2)) # 输出：/test?num=2
-    return	f'<h1>Test	page</h1> <p>{url_for('test_url_for')}<p>'
 
 
